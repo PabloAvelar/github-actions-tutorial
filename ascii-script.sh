2e484f0ca25bc2ffb15a09fefe0278ccb9e2be2c
Pablo Avelar Armenta <41934871+PabloAvelar@users.noreply.github.com>
2025-08-29 12:13:18 -0600

    misspelling

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,5 +1,5 @@
 sudo apt-get install cowsay -y
-coswsay -f dragon "Run for cover, I am a DRAGON... RAWR" >> dragon.txt
+cowsay -f dragon "Run for cover, I am a DRAGON... RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
